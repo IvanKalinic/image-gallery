4fecd6dca5837885b832d3a684db595e2ad54585
Ivan Kalinić <ivankalinic1998@gmail.com>
2022-03-07 17:14:33 +0100

    DSAADS

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,7 @@ const cors = require("cors");
 
 dotenv.config();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:8800', "myname"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
